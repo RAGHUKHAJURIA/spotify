57769594094cdae6cafe3c1533fc24e406926376
RAGHUKHAJURIA <khajuriaraghu0@gmail.com>
2024-12-22 23:43:26 +0530

    second

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 console.log('lets write java script');
+let currentSong = new Audio();
 
-async function getSongs(){
+async function getSongs() {
 
-    let a = await fetch("http://127.0.0.1:5500/songs/");
-    let response = await a.text();
-    console.log(response);
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    console.log(as);
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  console.log(as);
 
-    let songs = [];
-    for(let index = 0; index < as.length; index++){
-        const element = as[index];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href.split("/songs/")[1])
-        }
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1])
     }
+  }
 
-    return songs;
+  return songs;
 
+}
+
+const playMusic =(track) => {
+  // let audio = new Audio("/songs/" + track);
+  currentSong.src = "/songs/" + track;
+  currentSong.play();
 }
 
 
 
 async function main() {
 
-    // get the list of all songs
-    let songs = await getSongs();
-    console.log(songs);
 
-    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
-    for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> 
+
+  // get the list of all songs
+  let songs = await getSongs();
+  console.log(songs);
+
+  // show all song in the playlist
+  let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li> 
 
 
               <img src="music.svg" alt="">
@@ -43,19 +53,31 @@ async function main() {
               <div class="playnow">
                 <span>Play Now</span>
                 <img class="invert" src="playbtn.svg" alt="">
-              </div>
-       </li>`;
+              </div> </li>`;
+  }
+
+  // Attach an event listener to each song
+
+  Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e=>{
+    e.addEventListener("click", element=> {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML)
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    })
+ 
+  })
+
+  play.addEventListener("click", ()=>{
+    if(currentSong.paused){
+      currentSong.play();
+      play.src = "pause.svg"
     }
+    else {
+      currentSong.paused();
+      play.src = "playbtn.svg";
 
-    //play all songs
-    var audio = new Audio(songs[0]);
-    audio.play();
+    }
+  })
 
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(duration);
-        // The duration variable now holds the duration (in seconds) of the audio clip
-      });
 }
 
 main();
